6b9871d0e511355b08ff742c42d3f891091307a3
rlwing <rob@wingjr.com>
2020-02-25 13:25:45 -0700

    Test ShouldGetInitializedWithoutItemsOrTotalPrice passes

--- src/main/java/com/galvanize/cart/Cart.java
+++ src/main/java/com/galvanize/cart/Cart.java
@@ -1,4 +1,14 @@
 package com.galvanize.cart;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Cart {
+    public List<Item> getItems() {
+        return Collections.emptyList();
+    }
+
+    public double getTotalPrice() {
+        return 0.0;
+    }
 }
