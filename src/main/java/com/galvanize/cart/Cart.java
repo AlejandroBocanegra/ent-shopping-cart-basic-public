b6ff816ecec6ffb26eee314ccfdd5467050b2e7b
rlwing <rob@wingjr.com>
2020-02-25 13:54:44 -0700

    Test ShouldAllowAddingMoreThanOneOfAGivenItemToTheCart passes

--- src/main/java/com/galvanize/cart/Cart.java
+++ src/main/java/com/galvanize/cart/Cart.java
@@ -5,33 +5,48 @@ import java.util.Collections;
 import java.util.List;
 
 public class Cart {
-    List<Item> items = new ArrayList<>();
+    List<LineItem> items = new ArrayList<>();
 
-    public List<Item> getItems() {
+    public List<LineItem> getItems() {
         return items;
     }
 
     public double getTotalPrice() {
         double totalPrice = 0.0;
 
-        for(Item item : items){
-            totalPrice += item.getPrice();
+        for(LineItem item : items){
+            totalPrice += item.item.getPrice()*item.quantity;
         }
 
         return totalPrice;
     }
 
-    public void AddItem(Item item) {
-        items.add(item);
+    public void addItem(Item item, int quantity) {
+        items.add(new LineItem(item, quantity));
+    }
+
+    public void addItem(Item item){
+        addItem(item, 1);
     }
 
     public List<String> ItemizedList() {
         List<String> itemList = new ArrayList<>();
 
-        for (Item item : items){
-            itemList.add(item.toString());
+        for (LineItem item : items){
+            itemList.add(item.item.toString());
         }
 
         return itemList;
     }
+
+    class LineItem{
+        Item item;
+        int quantity;
+
+        public LineItem(Item item, int quantity) {
+            this.item = item;
+            this.quantity = quantity;
+        }
+    }
+
 }
--- src/test/java/com/galvanize/cart/CartTest.java
+++ src/test/java/com/galvanize/cart/CartTest.java
@@ -3,7 +3,6 @@ package com.galvanize.cart;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,10 +24,10 @@ public class CartTest {
          Item item1 = new Item("Fruit", 4.50);
          Item item2 = new Item("Ice Cream", 5.50);
 
-         cart.AddItem(item1);
+         cart.addItem(item1);
          assertEquals(4.50, cart.getTotalPrice());
 
-         cart.AddItem(item2);
+         cart.addItem(item2);
          assertEquals(10.00, cart.getTotalPrice());
      }
 
@@ -39,24 +38,24 @@ public class CartTest {
 
        Item item1 = new Item("Coffee", 2.5);
        Item item2 = new Item("Donuts", 4.5);
-       cart.AddItem(item1);
-       cart.AddItem(item2);
+       cart.addItem(item1);
+       cart.addItem(item2);
        assertEquals(new ArrayList<String>(Arrays.asList("Coffee - $2.5", "Donuts - $4.5")), cart.ItemizedList());
      }
 
-//     @Test
-//     public void ShouldAllowAddingMoreThanOneOfAGivenItemToTheCart() {
-//       Cart cart = new Cart();
-//
-//       Item item1 = new Item("Fruit", 4.50);
-//       Item item2 = new Item("Ice Cream", 5.50);
-//
-//       cart.AddItem(item1, 2);
-//       assertEquals(9.0, cart.getTotalPrice());
-//
-//       cart.AddItem(item2, 4);
-//       assertEquals(31.00, cart.getTotalPrice());
-//     }
+     @Test
+     public void ShouldAllowAddingMoreThanOneOfAGivenItemToTheCart() {
+       Cart cart = new Cart();
+
+       Item item1 = new Item("Fruit", 4.50);
+       Item item2 = new Item("Ice Cream", 5.50);
+
+       cart.addItem(item1, 2);
+       assertEquals(9.0, cart.getTotalPrice());
+
+       cart.addItem(item2, 4);
+       assertEquals(31.00, cart.getTotalPrice());
+     }
 
 //     @Test
 //     public void ShouldAllowYouToSeeHowManyOfEachItemIsInTheCart() {
